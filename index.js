d2219548c66b10362d86a91c3916272ae15645c1
mujtaba1016 <mujtaba1016@gmail.com>
2018-08-10 18:16:43 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// navigator.getMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+// navigator.getMedia({video:true, audio:false },function(stream) {
+navigator.mediaDevices.getUserMedia({video:true, audio:false },function(stream) {
+
+var Peer = require("simple-peer");
+var peer  = new Peer({
+	initiator: location.hash === '#inite',
+	trickle: false,
+	stream: stream
+});
+
+peer.on('signal',function(data) {
+	document.getElementById('yourId').value = JSON.stringify(data);
+});
+
+document.getElementById('connect').addEventListener('click',function(){
+	var otherId = JSON.parse(document.getElementById('otherId').value);
+	peer.signal(otherId);
+});
+
+
+document.getElementById('send').addEventListener('click', function() {
+	var yourMessage = document.getElementById('yourMessage').value;
+	peer.send(yourMessage);
+})
+
+
+peer.on('data',function (data) {
+	document.getElementById('messages').textContent += data + '\n';
+});
+
+peer.on('stream',function (stream) {
+	var video = document.createElement('video');
+	document.body.appendChild(video);
+	// video.src = window.URL.createObjectURL(stream);
+	video.srcObject = stream;
+	video.play(); 
+});
+
+
+},function(err) {
+	console.log(err);
+})
